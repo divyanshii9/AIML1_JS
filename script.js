5c752fdf181186a86fb05628556b4662dfc9324d
divyanshii9 <divyanshiibhatia9@gmail.com>
2026-03-09 22:42:05 +0530

    Lec_II

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
-// console.log("Hello World");
-// document.write("Hello World");
-var num=10;
-console.log(num);
-console.log(typeof(num));
-num = false;
-console.log(num);
-console.log(typeof(num));
-num = "Sanjeev Soni";
-console.log(num);
-console.log(typeof(num));
-num = null;
-console.log(num);
-console.log(typeof(num));
-num=undefined;
-console.log(num);
-console.log(typeof(num));
+// var a =10;
+// var b=20;
+// var linebreak= "<br/>";
 
-const sym1 = Symbol(4)
-console.log(sym1);
-const sym2 = Symbol(4);
-console.log(sym2);
-if(sym1==sym2){
-    console.log("true");
+// document.write("(a==b)=>");
+// result=(a<b);
+// document.write(result);
+// document.write(linebreak);
+
+// document.write("(a<b)=>");
+// result=(a<b);
+// document.write(result);
+// document.write(linebreak);
+
+// var count;
+// document.write("Starting loop"+"<br/>");
+// for(count=0;count<10;count++){
+//     document.write("current count:"+count);
+//     document.write("<br/>");
+// }
+// document.write("loop stopped!");
+
+const person={
+    fname:"Divyanshi",
+    lname:"Bhatia",
+    age:20,
 }
-else{
-    console.log("false")
+for(let x in person){
+    console.log("personal details:"+x+":"+person[x])
 }
-var a=100; var b=13; var c=10; var linebreak="<br>";
-document.write("a+b+c=");
-result=a+b+c;
-document.write(result);
-document.write(linebreak);
