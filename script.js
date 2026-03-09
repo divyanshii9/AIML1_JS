41cb2ceb80f708871a767ed52590f6f10e58a282
divyanshii9 <divyanshiibhatia9@gmail.com>
2026-03-09 22:39:52 +0530

    Day1

---
+++ script.js
@@ -0,0 +1,33 @@
+// console.log("Hello World");
+// document.write("Hello World");
+var num=10;
+console.log(num);
+console.log(typeof(num));
+num = false;
+console.log(num);
+console.log(typeof(num));
+num = "Sanjeev Soni";
+console.log(num);
+console.log(typeof(num));
+num = null;
+console.log(num);
+console.log(typeof(num));
+num=undefined;
+console.log(num);
+console.log(typeof(num));
+
+const sym1 = Symbol(4)
+console.log(sym1);
+const sym2 = Symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+    console.log("true");
+}
+else{
+    console.log("false")
+}
+var a=100; var b=13; var c=10; var linebreak="<br>";
+document.write("a+b+c=");
+result=a+b+c;
+document.write(result);
+document.write(linebreak);
