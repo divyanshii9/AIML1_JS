8ee64dee301991b38b8713cf99693c23802696ac
divyanshii9 <divyanshiibhatia9@gmail.com>
2026-03-09 22:47:32 +0530

    Lec_III

--- script.js
+++ script.js
@@ -1,30 +1,14 @@
-// var a =10;
-// var b=20;
-// var linebreak= "<br/>";
-
-// document.write("(a==b)=>");
-// result=(a<b);
-// document.write(result);
-// document.write(linebreak);
-
-// document.write("(a<b)=>");
-// result=(a<b);
-// document.write(result);
-// document.write(linebreak);
-
-// var count;
-// document.write("Starting loop"+"<br/>");
-// for(count=0;count<10;count++){
-//     document.write("current count:"+count);
-//     document.write("<br/>");
-// }
-// document.write("loop stopped!");
-
+var count ;
+    document.write("starting loop" +" <br/>");
+    for(count=0;count<10;count++){
+        document.write("current Count:" + count );
+        document.write("<br/>");
+    }
 const person={
-    fname:"Divyanshi",
-    lname:"Bhatia",
-    age:20,
-}
+    fname:"Ajay",
+    Lname:"singh",
+    age:25
+};
 for(let x in person){
-    console.log("personal details:"+x+":"+person[x])
+    console.log("person details:" + ":"+ person[x])
 }
