de14f670646a76f980d61cc2993c7ea338ebbce7
divyanshii9 <divyanshiibhatia9@gmail.com>
2026-03-12 02:17:30 +0530

    Assignment-1

---
+++ Assignment_1.js
@@ -0,0 +1,23 @@
+// 1. Program to demonstrate assignment.operator
+let num = 10;
+console.log("Initial value:", num); // Initial value: 10
+
+// += operator
+num += 5;
+console.log("After += 5:", num); // After += 5: 15
+
+// -= operator
+num -= 3;
+console.log("After -= 3:", num); // After -= 3: 12
+
+// *= operator
+num *= 2;
+console.log("After *= 2:", num); // After *= 2: 24
+
+// /= operator
+num /= 4;
+console.log("After /= 4:", num); // After /= 4: 6
+
+// %= operator
+num %= 3;
+console.log("After % 3:", num); // After % 3: 0
