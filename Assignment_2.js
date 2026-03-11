985afe00739b3cbd0a4ef6c5a57077f1df659afa
divyanshii9 <divyanshiibhatia9@gmail.com>
2026-03-12 02:21:02 +0530

    Assignment-2

---
+++ Assignment_2.js
@@ -0,0 +1,10 @@
+// 2. Program to find the largest of three numbers using ternery operator
+let a = 15;
+let b = 25;
+let c = 10;
+
+let largest = (a > b && a > c)
+          ? a
+          : (b > c ? b : c);
+
+console.log("Largest number is:", largest);
