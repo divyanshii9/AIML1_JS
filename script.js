c3e238e0ac0be29704d5f94893acc35d4a70392b
divyanshii9 <divyanshiibhatia9@gmail.com>
2026-03-20 09:45:31 +0530

    JSON

--- script.js
+++ script.js
@@ -1,24 +1,64 @@
-console.log("Hello World");
+// var text = '{"employees":[' +
+// '{"firstname":"John","lastname":"Doe"},' +
+// '{"firstname":"Anna","lastname":"Smith"},' +
+// '{"firstname":"Peter","lastname":"Jones"}]}';
 
-var myInfo=new Array();
-var myInfo1 = new Array;
-console.log(myInfo, myInfo1);
+// // Convert JSON string to object
+// var obj = JSON.parse(text);
+
+// // Display data
+// document.getElementById("Demo").innerHTML =
+//     obj.employees[0].firstname + " " + obj.employees[0].lastname;
+//     when the function is pass as an argument it is callled higher order function
+// Map METHOD
+
+var numbers=[1,2,3,4];
+var result=numbers.map(n=>n*2);
+console.log(result);
+
+// Filter Method
+var number=[30,50];
+var result=number.filter(n=>n>20);
+console.log(result);
+
+//Reduced Method
+var number=[10,20,30];
+var sum=number.reduce((total,n)=>total+n,0)
+console.log(sum);
 
 
-let collection=[
-    {},
-    [],
-    true,
-    "john",
-    function() {},
-    90076654334,
-    undefined,
-    null,
-    new String("abc"),
-    new Date(),
+//Promise Method
+var promise=new Promise((resolve,reject)=>{
+    let success=true;
+    if(success)
+    resolve("Data Loaded");
+else 
+reject("Error Occured");
+});
+promise.then(result=>console.log(result)).catch(error=>console.log(error));
 
-];
-console.log(collection);
-collection.teacherName="John mac";
-collection.phoneno=997867899;
-document.getElementById("myId")
+//Error Method
+try{
+    var x=y+10;
+}
+catch(error){
+    console.log(error.message);
+}
+
+//Local Storage allows data to be stored permanenetly in the browser
+// Store simple data
+localStorage.setItem("Name", "jeet");
+
+// Retrieve data
+var name = localStorage.getItem("Name");
+console.log(name);   // ✔ use 'name' not 'Name'
+
+// Store Object
+var user = { name: "Jeet", age: 50 };
+
+localStorage.setItem("user", JSON.stringify(user));  // ✔ setItem
+
+// Retrieve Object
+var data = JSON.parse(localStorage.getItem("user")); // ✔ localStorage spelling
+
+console.log(data);
