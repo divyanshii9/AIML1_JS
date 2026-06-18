c61aa721d15b865b81a919964c1b71aa51caf594
divyanshii9 <divyanshiibhatia9@gmail.com>
2026-06-19 00:27:14 +0530

    set_attribute

--- Dom.js
+++ Dom.js
@@ -1,29 +1,7 @@
-// function changeColor() {
-//     document.getElementById("para").style.color = "blue";
-// }git
-// function changeClass() {
-//     let x = document.getElementsByClassName("demo");
 
-//     for(let i=0;i<x.length;i++) {
-//         x[i].style.color = "red";
-//     }
-// }
-// function changeTag() {
-//     let p = document.getElementsByTagName("p");
-//     p[1].innerHTML = "Changed Paragraph";
-// }
-// function test() {
-//     let x = document.querySelector("#title");
-//     x.style.backgroundColor = "yellow";
-// }
-// function addHeading() {
+function setAttr() {
 
-//     let h2 = document.createElement("h2");
+    let element = document.getElementById("myH1");
 
-//     h2.innerText = "Hello DOM";
-
-//     document.body.append(h2);
-// }
-function removeElement() {
-    document.getElementById("demo").remove();
+    element.setAttribute("class","heading");
 }
