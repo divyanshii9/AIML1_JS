387cbe6bab63bb7ae58898c1de256c9db0a69fb4
divyanshii9 <divyanshiibhatia9@gmail.com>
2026-06-18 23:31:58 +0530

    js function

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-var grade = 'A';
+// var grade = 'A';
 
-document.write("Entering switch block<br>");
+// document.write("Entering switch block<br>");
 
-switch (grade) {
-    case 'A':
-        document.write("Good job<br>");
-        break;
+// switch (grade) {
+//     case 'A':
+//         document.write("Good job<br>");
+//         break;
 
-    case 'B':
-        document.write("Pretty good<br>");
-        break;
+//     case 'B':
+//         document.write("Pretty good<br>");
+//         break;
 
-    case 'C':
-        document.write("Passed<br>");
-        break;
+//     case 'C':
+//         document.write("Passed<br>");
+//         break;
 
-    case 'D':
-        document.write("Not so good<br>");
-        break;
+//     case 'D':
+//         document.write("Not so good<br>");
+//         break;
 
-    case 'F':
-        document.write("Failed<br>");
-        break;
+//     case 'F':
+//         document.write("Failed<br>");
+//         break;
 
-    default:
-        document.write("Unknown grade<br>");
+//     default:
+//         document.write("Unknown grade<br>");
+// }
+
+// document.write("Exiting switch block");
+function myFunction() {
+    alert("Hello World!");
 }
-
-document.write("Exiting switch block");
