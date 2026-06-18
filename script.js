73c07bb4799bd51e0828d3061e17381aa9b0a8ba
divyanshii9 <divyanshiibhatia9@gmail.com>
2026-06-18 23:30:13 +0530

    js switch

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-var age = 20;
+var grade = 'A';
 
-if (age > 18) {
-    document.write("<b>You are eligible to cast the vote.</b>");
+document.write("Entering switch block<br>");
+
+switch (grade) {
+    case 'A':
+        document.write("Good job<br>");
+        break;
+
+    case 'B':
+        document.write("Pretty good<br>");
+        break;
+
+    case 'C':
+        document.write("Passed<br>");
+        break;
+
+    case 'D':
+        document.write("Not so good<br>");
+        break;
+
+    case 'F':
+        document.write("Failed<br>");
+        break;
+
+    default:
+        document.write("Unknown grade<br>");
 }
+
+document.write("Exiting switch block");
