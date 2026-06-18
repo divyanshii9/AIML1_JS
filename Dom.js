7fbf41474c1eec22ca79f741a927229b8ac72685
divyanshii9 <divyanshiibhatia9@gmail.com>
2026-06-19 00:21:48 +0530

    class_name

--- Dom.js
+++ Dom.js
@@ -1,3 +1,10 @@
-function changeColor() {
-    document.getElementById("para").style.color = "blue";
+// function changeColor() {
+//     document.getElementById("para").style.color = "blue";
+// }git
+function changeClass() {
+    let x = document.getElementsByClassName("demo");
+
+    for(let i=0;i<x.length;i++) {
+        x[i].style.color = "red";
+    }
 }
