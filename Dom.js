fdf3eb90d4d98446c720c3a846c92ec4d9613dc7
divyanshii9 <divyanshiibhatia9@gmail.com>
2026-06-19 00:28:00 +0530

    double_clcik_event

--- Dom.js
+++ Dom.js
@@ -1,7 +1,10 @@
 
-function setAttr() {
+// function setAttr() {
 
-    let element = document.getElementById("myH1");
+//     let element = document.getElementById("myH1");
 
-    element.setAttribute("class","heading");
+//     element.setAttribute("class","heading");
+// }
+function changeColor() {
+    document.getElementById("text").style.color = "red";
 }
