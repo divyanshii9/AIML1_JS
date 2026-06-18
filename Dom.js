afeca2fc4d55c52152e03ebbb0a525a2459e52e2
divyanshii9 <divyanshiibhatia9@gmail.com>
2026-06-19 00:36:58 +0530

    Greeting user

--- Dom.js
+++ Dom.js
@@ -1,3 +1,11 @@
-function changeImage() {
-    document.getElementById("img").src = "dog.jpg";
+// function changeImage() {
+//     document.getElementById("img").src = "dog.jpg";
+// }
+function greet() {
+
+    let name =
+    document.getElementById("name").value;
+
+    document.getElementById("msg")
+    .innerHTML = "Hello " + name;
 }
